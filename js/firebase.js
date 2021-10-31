45c03097d2b0b7ad6d12230787287f5e940247cd
Keyuan01 <zhaokeyuan72@gmail.com>
2021-10-30 19:57:19 -0500

    add gitignore and modified firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -22,3 +22,26 @@
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
 </script>
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBoPSDdPBSj7IZbIHKc4yfBTszKkfUWwxE",
+  authDomain: "banco-de-tiempo-77b4e.firebaseapp.com",
+  databaseURL: "https://banco-de-tiempo-77b4e-default-rtdb.firebaseio.com",
+  projectId: "banco-de-tiempo-77b4e",
+  storageBucket: "banco-de-tiempo-77b4e.appspot.com",
+  messagingSenderId: "399972830225",
+  appId: "1:399972830225:web:064088a21e1d0cf30eb5b0",
+  measurementId: "G-J7Y6ZZT5YJ"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
